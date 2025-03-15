c80b3e9fce70cd37580bbd527c49b8eb7d4f7cdb
damon380 <damonchong380@gmail.com>
2025-03-15 11:03:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 import * as THREE from 'three';
 
-// Scene Setup
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-//const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('gameCanvas') });
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 const renderer = new THREE.WebGLRenderer();
-document.body.appendChild(renderer.domElement); // Append the renderer's canvas to the body
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Add a cube to the scene
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 camera.position.z = 5;
 
-// Animation Loop
 function animate() {
-  requestAnimationFrame(animate);
 
-  // Rotate the cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 
-  renderer.render(scene, camera);
+	renderer.render( scene, camera );
+
 }
-
-animate();
